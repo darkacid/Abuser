e2c91fc0dd25e92f0ffa5f47fc5df6aaa9bdef8b
Serg <sergstep314@gmail.com>
2018-02-12 23:02:51 +0400

    Update zimbraAuditParse.py

--- zimbraAuditParse.py
+++ zimbraAuditParse.py
@@ -2,3 +2,25 @@
 '''
 In dev...
 '''
+import re
+import datetime
+# Pattern definitions
+datePattern = "([0-9:\-\ ]{19})"
+
+
+
+def parseLine(logLine):
+
+    dateResult = re.search(datePattern,logLine)
+    if(dateResult):
+        eventDateStr = dateResult.group(1)
+        dateObject = datetime.datetime.strptime(eventDateStr,"%Y-%m-%d %H:%M:%S")
+        return dateObject
+    else:
+        return False    
+
+
+with open("audit.log") as auditFile:
+    for line in auditFile:
+        print (parseLine(line))
+        
