475c14aa9425e1df47c2dd7e737a88c0c7b1d3ee
Serg <sergstep314@gmail.com>
2018-03-09 16:25:47 +0400

    modify parser

--- zimbraAuditParse.py
+++ zimbraAuditParse.py
@@ -6,8 +6,7 @@ import re
 import datetime
 # Pattern definitions
 datePattern = "([0-9:\-\ ,]{23})" #First 23 characters of the log line
-
-
+IPPattern = ".*;oip=([0-9.]+);"
 
 def parseDate(logLine):
 
@@ -15,14 +14,29 @@ def parseDate(logLine):
     if(dateResult):
         eventDateStr = dateResult.group(1)
         dateObject = datetime.datetime.strptime(eventDateStr,"%Y-%m-%d %H:%M:%S,%f")
-        #dateobdateObject.isoformat(timespec="milliseconds")
         return dateObject
     else:
-        return False    
+        return False
+def parseIP(logLine):
+    IPResult = re.search(IPPattern,logLine)
+    if (IPResult):
+        IPResult=IPResult.group(1)
+        return IPResult
+    return False
+
+def parseEventType(logline):
+    if "oip=" in logline:
+        return True
+    
+
 
 filename = "audit.log"
 with open(filename) as auditFile:
     for line in auditFile:
-        parsedDate = (parseDate(line))
-        if(parsedDate > (datetime.datetime.today() - datetime.timedelta(days=30))):
-            print (parsedDate)
+        if(parseEventType(line)):
+            parsedIP = parseIP(line)
+            print(parsedIP)
+            parsedDate = (parseDate(line))
+            if(parsedDate > (datetime.datetime.today() - datetime.timedelta(days=30))):
+                pass
+                #print (parsedDate)
