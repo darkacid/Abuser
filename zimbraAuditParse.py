05845c110ea4ea5af4e9c831567ed9628fb20870
Serg <sergstep314@gmail.com>
2018-03-09 15:59:35 +0400

    modify date parse

--- zimbraAuditParse.py
+++ zimbraAuditParse.py
@@ -5,21 +5,24 @@ In dev...
 import re
 import datetime
 # Pattern definitions
-datePattern = "([0-9:\-\ ]{19})"
+datePattern = "([0-9:\-\ ,]{23})" #First 23 characters of the log line
 
 
 
-def parseLine(logLine):
+def parseDate(logLine):
 
     dateResult = re.search(datePattern,logLine)
     if(dateResult):
         eventDateStr = dateResult.group(1)
-        dateObject = datetime.datetime.strptime(eventDateStr,"%Y-%m-%d %H:%M:%S")
+        dateObject = datetime.datetime.strptime(eventDateStr,"%Y-%m-%d %H:%M:%S,%f")
+        #dateobdateObject.isoformat(timespec="milliseconds")
         return dateObject
     else:
         return False    
 
-
-with open("audit.log") as auditFile:
+filename = "audit.log"
+with open(filename) as auditFile:
     for line in auditFile:
-        print (parseLine(line))
+        parsedDate = (parseDate(line))
+        if(parsedDate > (datetime.datetime.today() - datetime.timedelta(days=30))):
+            print (parsedDate)
